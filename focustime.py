5cb4a29f789d9dd7ac8c880bb7700a509ee2196d
铜离子 <82830087+BHAWlA@users.noreply.github.com>
2023-06-08 01:43:24 +0800

    Create focustime.py

---
+++ focustime.py
@@ -0,0 +1,13 @@
+import time
+from playsound import playsound
+
+def focus_timer(duration):
+    print("开始专注工作！")
+    time.sleep(duration)  # 按指定时间段延时
+    playsound('path/to/alert_sound.mp3')  # 播放提醒音频
+    print("时间到！结束专注。")
+
+# 设置专注时长（以秒为单位）
+focus_duration = 1800  # 30分钟
+
+focus_timer(focus_duration)
